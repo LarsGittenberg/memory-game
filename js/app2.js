4c1e7b0e44f3e769a7f6b181655ccd22e62f3832
Lawrence Getubig <lawrencegetubig@lars-larsonson.home>
2018-05-11 22:39:11 -0400

    - change css, resize game panel to 500px - add two new bkg images in img folder

--- js/app2.js
+++ js/app2.js
@@ -130,7 +130,7 @@ function addCounter() {
 }
 */
 
-var moveCounter = [];
+var moveCounter = [];//an array designed to contain one closure function that increments counter
 function counterMaker() {
     var counter = 0;
     moveCounter.push(function() {
@@ -144,15 +144,9 @@ counterMaker();
 generateFullDeck();
 playMatchPair();
 
-
-
-
-
-
-
-
-
-
+$('.restart').on('click', function() {
+    location.reload();
+})
 
 
 /*
@@ -165,20 +159,6 @@ playMatchPair();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
